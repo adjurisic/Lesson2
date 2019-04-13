ce2c6e32f8ac232a8ba66fc0537e66730684a8cc
adjurisic <andrijadjurisic@gmail.com>
2019-04-13 23:03:36 +0200

    abc

--- src/examples/IntroToRobot.java
+++ src/examples/IntroToRobot.java
@@ -6,9 +6,11 @@ public class IntroToRobot {
 	public static void main(String[] args) {
 
 		Robot r2d2 = new Robot();
+		
 		r2d2.penDown();
 		r2d2.setPenWidth(20);
 		r2d2.setSpeed(5);
+		
 		for (int i = 0; i < 4; i++) {
 			r2d2.move(100);
 			r2d2.turn(90);
